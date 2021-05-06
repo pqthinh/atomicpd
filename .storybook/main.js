3166bce02093b4268402f1bfcf8cddc1ae80012e
pqthinh <phamquangquang2008@gmail.com>
2021-05-06 08:49:01 +0700

    init project

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,3 +2,13 @@ module.exports = {
 	stories: ["../components/**/*.stories.(js|mdx)"],
 	addons: ["@storybook/addon-docs"]
 };
+
+// presets.js
+// module.exports = [
+// 	{
+// 		name: "@storybook/addon-docs/react/preset",
+// 		options: {
+// 			configureJSX: true
+// 		}
+// 	}
+// ];
